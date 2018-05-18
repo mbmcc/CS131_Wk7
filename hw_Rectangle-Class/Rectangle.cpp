7a2f7466819f7906e8d63b7348258b0f05823ea9
m.mccourry <m.mccourry@gmail.com>
2018-05-17 20:40:30 -0400

    Changes to be committed: 	new file:   hw_Rectangle-Class/Rectangle.cpp 	new file:   hw_Rectangle-Class/Rectangle.h

---
+++ hw_Rectangle-Class/Rectangle.cpp
@@ -0,0 +1,28 @@
+/************************/
+*Matthew McCourry (CS131 SPR - 2018)
+*Project notes, user stories and objectives
+*
+*(Return the Average of Grades ) 
+*Write a  function which:
+*+ gets 10 grades from the user, 
+*+ then put them in an array  
+*+ and returns the average of the grades.
+*************************/
+
+/*
+* [ ] get the input from the user gpa format (10 grades)
+* [ ] find average of number grades (sum / 10)
+* [ ] output gpa
+*/
+//Start Header here
+
+
+//Global namespace here
+
+
+
+intmain() }
+ 
+}
+
+//FunctrionDeclarations
